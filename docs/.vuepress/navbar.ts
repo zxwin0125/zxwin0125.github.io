0461fe9647a80f5e10e9bde09e74da1ccab41d87
zxwin0125 <zxwin_0125@163.com>
2024-11-21 12:38:05 +0800

    fix: navbar

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -4,54 +4,103 @@ import { navbar } from "vuepress-theme-hope";
 // https://fontawesome.com/search?m=free&o=r
 // 专题话题的路径需在尾部添加 /，否则有可能出现链接错误。比如下方「生活」中的 baby/
 export default navbar([
-  { text: "博客", icon: "fa6-solid:blog", link: "/blog" },
+  { text: "主页", icon: "house", link: "/blog" },
   {
-    text: "应用",
-    icon: "fa6-solid:bars-staggered",
-    prefix: "/",
-    children: [
-      "apps/Applist",
-      {
-        text: "常用扩展",
-        icon: "fa6-brands:chrome",
-        link: "apps/Chrome",
-      },
-      {
-        text: "服务/专题",
-        icon: "",
-        prefix: "",
-        children: [
-          {
-            text: "专题示例",
-            icon: "fa6-solid:dice-d20",
-            link: "apps/topic/",
-          },
-        ],
-      },
-    ],
-  },
-  {
-    text: "生活",
-    icon: "fa6-solid:bed-pulse",
-    prefix: "/family/",
-    children: ["Diet", "Coupon"],
-  },
-  {
-    text: "工具",
-    icon: "fa6-solid:toolbox",
+    text: "知识体系",
+    icon: "sitemap",
+    prefix: "/knowledge/",
     children: [
       {
-        text: "ChatGPT SC",
-        icon: "fa6-solid:bolt",
-        link: "https://www.aishort.top/",
+        text: "前端",
+        icon: "code",
+        link: "/",
       },
-      { text: "IMGPrompt", icon: "fa6-solid:image", link: "https://prompt.newzone.top/" },
-      { text: "多语言翻译", icon: "fa6-solid:language", link: "https://tools.newzone.top/json-translate" },
       {
-        text: "工具收藏",
-        icon: "fa6-solid:bars",
-        link: "https://nav.newzone.top/",
+        text: "服务端",
+        icon: "server",
+        link: "/",
       },
-    ],
+      {
+        text: "客户端",
+        icon: "mobile",
+        link: "/",
+      },
+      {
+        text: "数据库",
+        icon: "database",
+        link: "/",
+      },
+      {
+        text: "系统",
+        icon: "computer",
+        link: "/",
+      },
+      {
+        text: "Mac相关",
+        icon: "laptop",
+        link: "/",
+      },
+      {
+        text: "工具相关",
+        icon: "toolbox",
+        link: "/",
+      },
+    ]
   },
+  { text: "项目相关", icon: "diagram-project", link: '/' },
+  { text: "工作技能", icon: "laptop-code", link: '/' },
+  {
+    text: "学会管理",
+    icon: "users-gear",
+    link: '/'
+  },
+  // {
+  //   text: "应用",
+  //   icon: "fa6-solid:bars-staggered",
+  //   prefix: "/",
+  //   children: [
+  //     "apps/Applist",
+  //     {
+  //       text: "常用扩展",
+  //       icon: "fa6-brands:chrome",
+  //       link: "apps/Chrome",
+  //     },
+  //     {
+  //       text: "服务/专题",
+  //       icon: "",
+  //       prefix: "",
+  //       children: [
+  //         {
+  //           text: "专题示例",
+  //           icon: "fa6-solid:dice-d20",
+  //           link: "apps/topic/",
+  //         },
+  //       ],
+  //     },
+  //   ],
+  // },
+  // {
+  //   text: "生活",
+  //   icon: "fa6-solid:bed-pulse",
+  //   prefix: "/family/",
+  //   children: ["Diet", "Coupon"],
+  // },
+  // {
+  //   text: "工具",
+  //   icon: "fa6-solid:toolbox",
+  //   children: [
+  //     {
+  //       text: "ChatGPT SC",
+  //       icon: "fa6-solid:bolt",
+  //       link: "https://www.aishort.top/",
+  //     },
+  //     { text: "IMGPrompt", icon: "fa6-solid:image", link: "https://prompt.newzone.top/" },
+  //     { text: "多语言翻译", icon: "fa6-solid:language", link: "https://tools.newzone.top/json-translate" },
+  //     {
+  //       text: "工具收藏",
+  //       icon: "fa6-solid:bars",
+  //       link: "https://nav.newzone.top/",
+  //     },
+  //   ],
+  // },
 ]);
