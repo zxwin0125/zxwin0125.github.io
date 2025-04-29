24fe9412e5229da2261f5c227b03eb96155ad741
zxwin0125 <zxwin_0125@163.com>
2025-04-29 10:15:40 +0800

    perf: vuepress 迁移 vitepress

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,33 @@
+import { defineConfig } from 'vitepress';
+
+export default defineConfig({
+  lang: 'zh-CN',
+  title: 'VitePress',
+  description: 'Vite & Vue powered static site generator.',
+  themeConfig: {
+    nav: [
+      { text: 'Example', link: '/example' },
+
+      // {
+      //   text: 'Dropdown Menu',
+      //   items: [
+      //     { text: 'Item A', link: '/item-1' },
+      //     { text: 'Item B', link: '/item-2' },
+      //     { text: 'Item C', link: '/item-3' },
+      //   ],
+      // },
+
+      // ...
+    ],
+
+    sidebar: [
+      {
+        // text: 'Guide',
+        items: [
+          { text: 'Example', link: '/example' },
+          // ...
+        ],
+      },
+    ],
+  },
+});
