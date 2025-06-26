6afbcb4b71c5028ad3f97ae03d0261d3e0b903ae
zxwin <zxwin_0125@163.com>
2025-06-26 10:16:23 +0800

    fix: giscus comment
    comment bug
    
    fix #4

--- env.d.ts
+++ env.d.ts
@@ -6,14 +6,6 @@ declare module 'vitepress' {
   export namespace DefaultTheme {
     export interface Config {
       /**
-       * 访客统计配置
-       */
-      visitor?: {
-        /** 统计 id（单独页面的统计会作为前缀使用）*/
-        badgeId: string
-      }
-
-      /**
        * giscus 评论配置
        *  请根据 https://giscus.app/zh-CN 生成内容填写
        */
