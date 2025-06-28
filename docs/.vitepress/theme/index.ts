45af93e0ba85994c4105873fd33691fbeec5953e
zxwin0125 <zxwin_0125@163.com>
2025-06-28 12:04:49 +0800

    fix: add theme color
    add theme color
    
    fix #8

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,8 +1,12 @@
-import { h } from 'vue'
-import { useData } from 'vitepress'
+import { h, watch } from 'vue'
+import { useData, EnhanceAppContext } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 
 import ZLayout from './components/ZLayout.vue'
+
+import './styles/index.scss'
+
+let homePageStyle: HTMLStyleElement | undefined
 
 export default {
   extends: DefaultTheme,
@@ -17,5 +21,42 @@ export default {
     }
 
     return h(ZLayout, props)
+  },
+  enhanceApp({ app, router }: EnhanceAppContext) {
+    app.provide('DEV', process.env.NODE_ENV === 'development')
+
+    if (typeof window !== 'undefined') {
+      watch(
+        () => router.route.data.relativePath,
+        () => updateHomePageStyle(location.pathname === '/'),
+        { immediate: true }
+      )
+    }
+  }
+}
+
+if (typeof window !== 'undefined') {
+  // detect browser, add to class for conditional styling
+  const browser = navigator.userAgent.toLowerCase()
+  if (browser.includes('chrome')) document.documentElement.classList.add('browser-chrome')
+  else if (browser.includes('firefox')) document.documentElement.classList.add('browser-firefox')
+  else if (browser.includes('safari')) document.documentElement.classList.add('browser-safari')
+}
+
+function updateHomePageStyle(value: boolean) {
+  if (value) {
+    if (homePageStyle) return
+
+    homePageStyle = document.createElement('style')
+    homePageStyle.innerHTML = `
+    :root {
+      animation: rainbow 12s linear infinite;
+    }`
+    document.body.appendChild(homePageStyle)
+  } else {
+    if (!homePageStyle) return
+
+    homePageStyle.remove()
+    homePageStyle = undefined
   }
 }
